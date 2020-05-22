b50c4a580d1cd19d07b9a68fa11ade2c59d8a661
babanayak <jarupulababa@gmail.com>
2020-05-22 18:38:34 +0530

    Junit

--- src/test/java/com/EstimatedDeliveryDate/Test/DataAccessTest.java
+++ src/test/java/com/EstimatedDeliveryDate/Test/DataAccessTest.java
@@ -1,21 +1,34 @@
 package com.EstimatedDeliveryDate.Test;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import com.EstimatedDeliveryDate.Entity.CriticalPullTime;
 import com.EstimatedDeliveryDate.Entity.CutOffTime;
 import com.EstimatedDeliveryDate.Entity.Holiday;
 import com.EstimatedDeliveryDate.Entity.Holidays;
 import com.EstimatedDeliveryDate.Entity.TransitTime;
+import com.EstimatedDeliveryDate.Repository.EstimatedDeliveryDateRepositoryImpl;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
+@ExtendWith(MockitoExtension.class)
 public class DataAccessTest {
 	private Holiday holiday1;
 	private Holiday holiday2;
@@ -24,28 +37,31 @@ public class DataAccessTest {
 	private CutOffTime cutofftime1;
 	private CutOffTime cutofftime2;
 	private TransitTime transittime;
+    private DynamoDBMapper mapper;
+    @InjectMocks 
+    private EstimatedDeliveryDateRepositoryImpl repository;
 
-	@Before
+	@BeforeEach
 	public void setUp() {
-		holiday1 = new Holiday();
-		holiday2 = new Holiday();
-		holidays = new Holidays();
-		cpt = new CriticalPullTime();
-		cutofftime1 = new CutOffTime();
-		cutofftime2 = new CutOffTime();
-		transittime = new TransitTime();
+		mapper=mock(DynamoDBMapper.class);
+		repository=new EstimatedDeliveryDateRepositoryImpl();
+		repository.setMapper(mapper);
 	}
 
 	@Test
 	public void testHoliday() {
-		holiday1.setDeliveryHoliday(0);
-		holiday1.setPickUpHoliday(1);
-		holiday1.setProcessingHoliday(1);
+		holiday1 = new Holiday();
+		holiday2 = new Holiday();
+		holidays = new Holidays();
+		
+		holiday1.setDeliveryHoliday(false);
+		holiday1.setPickUpHoliday(true);
+		holiday1.setProcessingHoliday(true);
 		holiday1.setHolidayDate("2020-05-22");
 
-		holiday2.setDeliveryHoliday(1);
-		holiday2.setPickUpHoliday(0);
-		holiday2.setProcessingHoliday(1);
+		holiday2.setDeliveryHoliday(true);
+		holiday2.setPickUpHoliday(false);
+		holiday2.setProcessingHoliday(true);
 		holiday2.setHolidayDate("2020-05-23");
 
 		List<Holiday> holidaylist = new ArrayList<Holiday>();
@@ -53,17 +69,17 @@ public class DataAccessTest {
 		holidaylist.add(holiday2);
 		holidays.setHolidayDatesList(holidaylist);
 		holidays.setShippingServiceId("ROYAL_MAIL_METHOD_1");
-
-		assertEquals(holidays.getShippingServiceId(), "ROYAL_MAIL_METHOD_1");
-		assertEquals(holidays.getHolidayDatesList().get(0).getHolidayDate(), "2020-05-22");
-		assertEquals(holidays.getHolidayDatesList().get(1).getHolidayDate(), "2020-05-23");
-		assertEquals(holidays.getHolidayDatesList().get(0).getDeliveryHoliday(), (Integer) 0);
-		assertEquals(holidays.getHolidayDatesList().get(1).getPickUpHoliday(), (Integer) 0);
-		assertEquals(holidays.getHolidayDatesList().get(0).getProcessingHoliday(), (Integer) 1);
+		
+        when(mapper.load(Holidays.class,"ROYAL_MAIL_METHOD_1")).thenReturn(holidays);
+        assertEquals(repository.getHolidays("ROYAL_MAIL_METHOD_1"),holidays);
 	}
 
 	@Test
 	public void testCPT() {
+		cpt = new CriticalPullTime();
+		cutofftime1 = new CutOffTime();
+		cutofftime2 = new CutOffTime();
+		
 		cutofftime1.setCutoffTime("20:00");
 		cutofftime1.setProcessingDay("WEDNESDAY");
 
@@ -80,15 +96,15 @@ public class DataAccessTest {
 		cpt.setDeliveryDays(deliverydaylist);
 		cpt.setProcessingDayCutoffList(cptlist);
 		cpt.setShippingServiceId("ROYAL_MAIL_METHOD_1");
-
-		assertEquals(cpt.getShippingServiceId(), "ROYAL_MAIL_METHOD_1");
-		assertEquals(cpt.getProcessingDayCutoffList().get(0).getProcessingDay(), "WEDNESDAY");
-		assertEquals(cpt.getDeliveryDays().get(0), "MONDAY");
+       
+		when(mapper.load(CriticalPullTime.class,"ROYAL_MAIL_METHOD_1")).thenReturn(cpt);
+		assertEquals(repository.getCPT("ROYAL_MAIL_METHOD_1"),cpt);
 
 	}
 
 	@Test
 	public void testTransitTime() {
+		transittime = new TransitTime();
 		transittime.setShippingServiceId("ROYAL_MAIL_METHOD_1");
 		transittime.setTransitTime(840);
 		Map<String, Integer> map = new HashMap<String, Integer>();
@@ -99,8 +115,7 @@ public class DataAccessTest {
 		List<Map<String, Map<String, Integer>>> transitoverridemap = new ArrayList<Map<String, Map<String, Integer>>>();
 		transitoverridemap.add(transitmap);
 
-		assertEquals(transittime.getTransitTime(), (Integer) 840);
-		assertEquals(transittime.getShippingServiceId(), "ROYAL_MAIL_METHOD_1");
-
+		when(mapper.load(TransitTime.class,"ROYAL_MAIL_METHOD_1")).thenReturn(transittime);
+		assertEquals(repository.getTransitTime("ROYAL_MAIL_METHOD_1"),transittime);
 	}
 }
